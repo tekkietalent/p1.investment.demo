e7bf8a609ff9c6ad092c34c6358528206b3ff169
Tom Martin <tom@qubitsoft.co.uk>
2021-04-30 16:31:41 +0100

    Initial project commit, with basic template structure [pre refactoring] and an initial data access code to sanity test api access.

--- P1.Interview.UI/Program.cs
+++ P1.Interview.UI/Program.cs
@@ -1,11 +1,8 @@
+using MediatR;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace P1.Interview.UI
@@ -18,6 +15,8 @@ namespace P1.Interview.UI
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMediatR(typeof(Program));
 
             await builder.Build().RunAsync();
         }
